0c07349b3f133dbaa9d70e82b5a2a33f847cbe85
Karinkarllina <karinkhaliullina@gmail.com>
2023-08-09 13:04:25 +0300

    Тести

--- models/auth-controller.test.js
+++ models/auth-controller.test.js
@@ -1,10 +1,10 @@
 import mongoose from 'mongoose';
+import request from 'supertest';
 import 'dotenv/config';
 import app from "../app.js";
-import request from 'supertest';
 import User from './user.js'
 
-const { PORT, DB_HOST_TEST } = process.env;
+const {PORT, DB_HOST_TEST} = process.env;
 
 describe("test login route", () => { 
 
@@ -13,32 +13,36 @@ describe("test login route", () => {
     beforeAll(async () => {
         await mongoose.connect(DB_HOST_TEST);
         server = app.listen(PORT);
-    })
+    });
     
-    afterAll(async ()=> {
+    afterAll(async () => {
         await mongoose.connection.close();
         server.close();
-    })
+    });
 
-    afterEach(async()=> {
+    afterEach(async () => {
         await User.deleteMany({});
-    })
+    });
 
     test("test login with correct data", async () => {
         
         const loginData = {
-            "email": "Nadin@email.com",
+            "email": "Naduin@email.com",
             "password": "12345678"
         };
-        const { statusCode, body } = await request(app).post("/api/auth/login").send(loginData);
-        
+        const {statusCode, body} = await request(app).post("/api/auth/login").send(loginData);
+
         expect(statusCode).toBe(200);
         expect(body.token).toBeDefined(); 
+        
         expect(body.user.email).toBe(loginData.email);
 
         const user = await User.findOne({ email: loginData.email }); 
+
         expect(user.email).toBe(loginData.email);
+        expect(typeof user.email).toBe('string');
         expect(body.user.subscription).toBeDefined();
+        expect(typeof user.subscription).toBe('string');
      })
     
 });
